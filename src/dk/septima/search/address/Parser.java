782187af970b5f9c3e9b3f63eaeeb6c430280a98
klavs <klavs@septima.dk>
2017-08-22 15:11:33 +0200

    Parser litra korrekt - blank->"a"

--- src/dk/septima/search/address/Parser.java
+++ src/dk/septima/search/address/Parser.java
@@ -35,15 +35,18 @@ public class Parser {
 		}else{
 			//�����
 			String pat_streetname = "([A-Za-z0-9ÆØÅæøåÉÜÄÖÈéüäöèÿ\\.\\-\\ ''\\/\\(\\)]+[A-Za-zÆØÅæøåÉÜÄÖÈéüäöèÜÿ]+)";
-			Pattern[] patterns = new Pattern[3];;
+			Pattern[] patterns = new Pattern[4];
 			patterns[0] = Pattern.compile(pat_streetname + "[,\\ ]+([0-9]{1,3} *[A-Za-z]{0,1})[,]*$"); // Streetname (w/o ','), streetbuildingidentifier and nothing afterwards
 			patterns[1] = Pattern.compile(pat_streetname + "[,\\ ]+([0-9]{1,3} *[A-Za-z]{0,1}),{0,1} [A-Za-zØÅæøåÉÜÄÖÈéüäöèÿ\\,\\ ]*$"); //Ramsherred 13 Br�ndstrup, flyttet ned da den overmatcher etager mv.
 			patterns[2] = Pattern.compile(pat_streetname + "[,\\ ]+([0-9]{1,3} *[A-Za-z]{0,1})[,\\ ]+[A-Za-z0-9ØÅæøåÉÜÄÖÈéüäöèÿ\\.\\-\\ ''\\/\\(\\)]*"); //Skolegade 9 A, �ster H�jst-
-			//patterns[3] = Pattern.compile(pat_streetname + "[ ]*$");
+			patterns[3] = Pattern.compile(pat_streetname + "[ ]*$");
 			for (int i=0;i<patterns.length;i++){
 				Matcher matcher = patterns[i].matcher(adrWork);
 				if (matcher.find()){
-					parseResult.streetName = matcher.group(1);
+					if (parseResult.streetName == ""){
+						parseResult.streetName = matcher.group(1);
+						parseResult.streetbuildingidentifier = "";
+					}
 					if (matcher.groupCount() >= 2){
 						parseResult.streetbuildingidentifier = matcher.group(2);
 					}
@@ -56,26 +59,22 @@ public class Parser {
 	
 	
 	public static void main(String[] args) {
-//		System.out.println(Parser.parse("A 444 94 Ucklum").toString());
-//		System.out.println(Parser.parse("A 444 94").toString());
-//		System.out.println(Parser.parse("A").toString());
-//		System.out.println(Parser.parse("Amdal 123B 444 94 Ucklum").toString());
-//		System.out.println(Parser.parse("Amdal 123 444 94").toString());
-//		System.out.println(Parser.parse("Amdal 123").toString());
-//		System.out.println(Parser.parse("Amdal").toString());
-//		System.out.println(Parser.parse("Annas v�g 123 444 94 Ucklum").toString());
-//		System.out.println(Parser.parse("Annas v�g 123 444 94").toString());
-//		System.out.println(Parser.parse("Annas v�g 123").toString());
-		System.out.println(Parser.parse("Annas v�g").toString());
-//		System.out.println(Parser.parse("Amdal 123 (444 94 Ucklum)").toString());
-//		System.out.println(Parser.parse("Amdal 123 (444 94").toString());
-//		System.out.println(Parser.parse("Amdal 123 (").toString());
-//		System.out.println(Parser.parse("Amdal").toString());
-//		System.out.println(Parser.parse("Annas v�g 123C 44494 Ucklum").toString());
-//		System.out.println(Parser.parse("Annas v�g 123 444 94").toString());
-//		System.out.println(Parser.parse("Annas v�g 123C").toString());
-		System.out.println(Parser.parse("AGNES V�G").toString());
-		System.out.println(Parser.parse("Agnes v�g").toString());
+		System.out.println(Parser.parse("A").toString());
+		System.out.println(Parser.parse("A 444 94").toString());
+		System.out.println(Parser.parse("A 444 94 Ucklum").toString());
+		System.out.println(Parser.parse("Agnes Väg").toString());
+		System.out.println(Parser.parse("Agnes Väg 123").toString());
+		System.out.println(Parser.parse("Agnes Väg 123B").toString());
+		System.out.println(Parser.parse("Agnes Väg 123 444 94").toString());
+		System.out.println(Parser.parse("Agnes Väg 123 444 94 Ucklum").toString());
+		System.out.println(Parser.parse("Agnes Väg 123B 444 94 Ucklum").toString());
+		System.out.println(Parser.parse("Agnes Väg 444 94 Ucklum").toString());
+     	System.out.println(Parser.parse("Annas väg").toString());
+		System.out.println(Parser.parse("Annas väg 123").toString());
+		System.out.println(Parser.parse("Annas väg 123 444 94").toString());
+		System.out.println(Parser.parse("Annas väg 444 94 Ucklum").toString());
+		System.out.println(Parser.parse("AGNES VäG").toString());
+		System.out.println(Parser.parse("Agnes Väg").toString());
 	}
 
 }
